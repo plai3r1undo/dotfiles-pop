9a8c75820f48d87e0737e2758e7c8fd20d7b1309
plai3r1undo <90139041+plai3r1undo@users.noreply.github.com>
2024-01-10 11:27:09 +0100

    quality of life

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-feh --bg-fill ~/Pictures/wallpapers/wallpaper.jpg
+feh --bg-fill ~/Pictures/wallpapers/wallhaven-skeleton.jpg
 
+# picom is a x compositore therefore it won't finish executing or at least that's what I think
+# migh cause problems
 slstatus &
 picom &
+sxhkd &
